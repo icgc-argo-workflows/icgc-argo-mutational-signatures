3c6dd26757e596bb86fee6c06839219e9f565425
Felix Beaudry <Felix.Beaudry@oicr.on.ca>
2023-02-20 14:46:47 -0500

    thresholder finalize (and new data)

--- error_thresholding/error_thresholder.R
+++ error_thresholding/error_thresholder.R
@@ -5,44 +5,64 @@ library(jsonlite)
 library(signature.tools.lib)
 
 library(philentropy)
-library(statip)
 library(Metrics)
+library(dplyr)
+library(topicmodels)
+
+#### INPUT ####
 
 option_list = list(
-  make_option(c("-j", "--input_json"), type="character", default=NULL,help="JSON from upstream", metavar="character"),
- # make_option(c("-e", "--error_threshold"), type="character", default=NULL,help="JSON from upstream", metavar="character"),
+  make_option(c("-i", "--input"), type="character", default=NULL,help="input from upstream", metavar="character"),
   make_option(c("-s", "--signature_matrix"), type="character", default=NULL,help="matrix from which the signatures were calculated", metavar="character")
 )
 
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
 
+input_location <- opt$input
+signature_matrix_location <- opt$signature_matrix
+
+##import data
 ##test data
-setwd('~/Documents/Github')
-input_json <- read_json('icgc-argo-mutational-signatures/signaturetoolslib/tests/expected/test_assignments.json')
-signature_matrix <- 
-#error_threshold <- 1000
+#signature_matrix <-  'icgc-argo-mutational-signatures/error_thresholding/data/COSMIC_v2_SBS_GRCh38.txt'
+#input_json <- 'icgc-argo-mutational-signatures/signaturetoolslib/tests/expected/test_assignments.json'
 
-catalog <- input_json$catalogues[[1]]
+signature_matrix <-  read.table(signature_matrix_location,header = T)
+input_json <- read_json(input_location) #signature.tools.lib outputs json
 
-exposures <- input_json$exposures[[1]]
-predicted <- exposures * signature_matrix
+#reformat inputs
+catalog <- unlist(input_json$catalogues[[1]])
+exposures <- as.data.frame(input_json$exposures[[1]])
 
-## Calculate Differences
+#predict from exposures and signature matrix
+predicted_mat <- t(exposures) * signature_matrix[,-1]
+predicted_sum <- rowSums(predicted_mat) 
+names(predicted_sum) <- signature_matrix[,1]
+
+#### CALCULATE ####
 
 ### Kullback-Leibler Divergence // beta divergence
-kl_data <- philentropy::KL(x = cbind.data.frame(catalog,predicted),est.prob = TRUE)
-
-### Hellinger Distance
-hellinger_data <- statip::hellinger(catalog,predicted)
+kl_data <- philentropy::KL(x = rbind(catalog/sum(catalog),predicted_sum/sum(predicted_sum)))
 
 ### root mean square error (RMSE)
-rmse_data <- Metrics::rmse(catalog,predicted)
+rmse_data <- Metrics::rmse(catalog,predicted_sum)
 
 ### Normal error, sum Absolute of differences (SAD)
-data.frame(catalog,predicted) %>% mutate(SAD = abs(catalog) + abs(predicted))
+SAD_table <- data.frame(catalog,predicted_sum) %>% 
+                          mutate(SAD = abs(catalog) + abs(predicted_sum))
+SAD_data  <- sum(SAD_table$SAD)
 
+### Hellinger Distance
+hellinger_data <- topicmodels::distHellinger(t(as.data.frame(catalog)),t(as.data.frame(predicted_sum)))
 
+#### OUTPUT ####
 
+#stick all the results together
+all.results <- list(kl_data[[1]],rmse_data,SAD_data,unlist(hellinger_data[1]))
+names(all.results) <- c("KL","RMSE","SAD","Hellinger")
 
+#conver to JSON and write
+ListJSON <- jsonlite::toJSON(all.results,pretty=TRUE,auto_unbox=TRUE)
+
+write(ListJSON,file = paste("errors.json",sep=""))
 
